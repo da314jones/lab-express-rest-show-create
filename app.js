ed03f1995d001af923f728d5d6abe0b922c2f434
DJones <dwaynejones@Dwaynes-Air.mynetworksettings.com>
2023-10-14 20:40:36 -0400

    basic crud functionality

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require("express");
+const models = require("./models/models");
+const createLog = require("./controllers/logs/create");
+const readLog = require("./controllers/logs/read");
+const updateLog = require("./controllers/logs/update");
+const deleteLog = require("./controllers/logs/delete");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send(`This is your server running here.`);
+    res.send(`Welcome to The Captains Log.`);
 });
 
-module.exports = app 
+// Index route shows all indices of a particular resource
+app.get("/logs", (req, res) => {
+    res.json(models);
+});
+
+// Show route for a single entry point based on a wildcard ("/prefix/:('user-Defined-variable')") identifier
+app.get("/logs/:index", readLog);
+
+// Create route
+app.post("/logs", createLog);
+
+// Update route
+app.put("/logs/:index", updateLog);
+
+// Delete route
+app.delete("/logs/:index", deleteLog);
+
+// Middleware error handling always at the bottom of the stack
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(`Sumting BreK!`);
+});
+
+module.exports = app;
