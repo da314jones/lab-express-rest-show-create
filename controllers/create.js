9df9fbe3b21898e9736c90fe21f7f10fbd0a9b40
DJones <dwaynejones@pursuit.com>
2023-12-05 19:09:58 -0500

    finished for resubmit

--- controllers/create.js
+++ controllers/create.js
@@ -1,15 +1,16 @@
 const models = require("../models/models");
 
 const create = (req, res, next) => {
-    const newLog = req.body;
-    if (!newLog.captainName || !newLog.title || !newLog.post) {
-        const error = new Error("Missing required fields");
-        error.status = 400;
-        return next(error);
-    }
-    models.push(newLog);
-    console.log("Added new log entry:", newLog);
-    res.json(newLog);
+  const newLog = req.body;
+  if (!newLog.captainName || !newLog.title || !newLog.post) {
+    const error = new Error("Missing required fields");
+    error.status = 400;
+    return next(error);
+  }
+  newLog.id = models.length + 1;
+  models.push(newLog);
+  console.log("Added new log entry:", newLog);
+  res.status(201).json(newLog);
 };
 
 module.exports = create;
--- controllers/read.js
+++ controllers/read.js
@@ -1,11 +1,11 @@
 const models = require("../models/models");
 
-const read = ("/:index", (req, res) => {
+const read = ("/logs/:index", (req, res) => {
     const idx = parseInt(req.params.index);
     if (idx >= 0 && idx < models.length) {
         res.json(models[idx]);
     } else {
-        res.redirect(404).json({ message: "Log not found" });
+        res.status(404).json({ message: "Log not found" });
     }
 });
 
